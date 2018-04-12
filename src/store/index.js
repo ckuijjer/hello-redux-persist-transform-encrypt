7ca28d2d3f9ab5d21a96c50e2f267ac03b379ef8
Casper Kuijjer <casper@kuijjer.com>
2018-04-12 20:44:43 +0200

    encrypt redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+import createEncryptor from 'redux-persist-transform-encrypt'
 
 import counter from './counter'
 
@@ -8,9 +9,16 @@ const reducers = combineReducers({
   counter,
 })
 
+const encryptor = createEncryptor({
+  secretKey: 'my-super-secret-key',
+  onError: err =>
+    console.error(`redux-persist-transform-encrypt error: ${err}`),
+})
+
 const persistConfig = {
   key: 'root',
   storage,
+  transforms: [encryptor],
 }
 
 const persistedReducer = persistReducer(persistConfig, reducers)
