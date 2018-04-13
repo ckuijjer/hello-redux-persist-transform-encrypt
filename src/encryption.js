6690ac210047a29309458e5b42a14e9a02af7a1a
Casper Kuijjer <casper@kuijjer.com>
2018-04-13 11:22:34 +0200

    Added some logging to understand what the persistConfig transforms do

--- src/encryption.js
+++ src/encryption.js
@@ -4,7 +4,8 @@ const ENCRYPTION_KEY = 'persist:encryptionKey'
 
 const cookies = new Cookies()
 
-export const getEncryptionKey = () => cookies.get(ENCRYPTION_KEY) || ''
+export const getEncryptionKey = () =>
+  cookies.get(ENCRYPTION_KEY) || 'my secret key'
 
 export const setEncryptionKey = ({ encryptionKey, maxAge = 60 }) =>
   cookies.set(ENCRYPTION_KEY, encryptionKey, { path: '/', maxAge })
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, createTransform } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 import createEncryptor from 'redux-persist-transform-encrypt'
 
@@ -11,16 +11,49 @@ const reducers = combineReducers({
   counter,
 })
 
+const secretKey = getEncryptionKey()
+console.log('🔑', secretKey)
+
 const encryptor = createEncryptor({
-  secretKey: getEncryptionKey(),
-  onError: err =>
-    console.error(`redux-persist-transform-encrypt error: ${err}`),
+  secretKey,
+  onError: err => {
+    console.error(`redux-persist-transform-encrypt error: ${err}`)
+  },
 })
+
+const createLogger = (prefix, keyInterest) =>
+  createTransform(
+    (state, key) => {
+      if (key === keyInterest) console.log(`${prefix} persist ${key}`, state)
+      return state
+    },
+    (state, key) => {
+      if (key === keyInterest) console.log(`${prefix} rehydrate ${key}`, state)
+      return state
+    },
+  )
+
+const nullStateRemover = createTransform(
+  state => state, // when persisting don't do anything
+  (state, key) => {
+    if (key === 'counter' && state === null) {
+      console.log('trying to remove the counter')
+      return undefined
+    } else {
+      return state
+    }
+  },
+)
 
 const persistConfig = {
   key: 'root',
   storage,
-  transforms: [encryptor],
+  transforms: [
+    createLogger('A', 'counter'),
+    // nullStateRemover,
+    encryptor,
+    createLogger('B', 'counter'),
+  ], // persist is left to right, rehydrate is right to left
 }
 
 const persistedReducer = persistReducer(persistConfig, reducers)
